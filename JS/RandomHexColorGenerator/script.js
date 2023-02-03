b2770eb19b808b766eaa26475c1fbe4f1a57bb6b
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-02-03 11:09:26 -0400

    JS for RandomHexColorGenerator

--- JS/RandomHexColorGenerator/script.js
+++ JS/RandomHexColorGenerator/script.js
@@ -1,17 +1,13 @@
-const number = document.querySelector(".random-number");
-const generateNumberButton = document.querySelector(".generate-number");
+const color = document.querySelector(".random-color");
+const generateColorButton = document.querySelector(".generate-color");
 
-const INCLUSIVE_LOWER_LIMIT = 0;
-const EXCLUSIVE_UPPER_LIMIT = 11;
+const generateRandomColor = () => {
+	const randomColor = `#${Math.random().toString(16).substring(2, 8)}`;
 
-const generateRandomNumber = () => {
-	const randomNumber = Math.floor(
-		Math.random() * EXCLUSIVE_UPPER_LIMIT + INCLUSIVE_LOWER_LIMIT,
-	);
-
-	number.innerHTML = randomNumber;
+	color.innerHTML = randomColor;
+	document.body.style.backgroundColor = randomColor;
 };
 
-generateRandomNumber();
+generateRandomColor();
 
-generateNumberButton.addEventListener("click", generateRandomNumber);
+generateColorButton.addEventListener("click", generateRandomColor);
